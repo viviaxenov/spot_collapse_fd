1b1e93a0350eba03c3b38fa8e35f237a67810b97
viviaxenov <viviaxenov@gmail.com>
2021-12-13 14:38:12 +0300

    Started working on Poisson solver

---
+++ PoissonSolver.py
@@ -0,0 +1,8 @@
+import numpy as np
+import scipy as sp
+
+def solve(h_x, h_y, rhs: np.ndarray):
+    """
+        Solves the finite difference version of Poisson equation (u_i+1,j - 2u_i,j + u_i-1,j)/h_x**2 + (u_i,j+1 - 2u_i,j + u_i,j-1)/h_y**2 = rhs_i,j
+        """
+    pass
