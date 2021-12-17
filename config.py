a metric ton of fixes in numerical scheme

config.py
@@ -16,7 +16,7 @@ T = 1.0
 
 g = 9.81  # acceleration of gravity
 rho_0 = 1.0
-Lambda = 0.1
+Lambda = 100.
 N = 1.0
 Tb = 2 * np.pi / N
 
@@ -28,4 +28,4 @@ Re = 100.0  # Reynolds number
 Fr = 0.1  # Froude number
 Sc = 709.2  # Schmidt number
 
-poisson_solver_defaults = {"max_iter": 5000, "batch_size": 7, "eps": 1e-3}
+poisson_solver_defaults = {"max_iter": 5000, "batch_size": 7, "eps": 1e-7}

utils.py
@@ -22,8 +22,8 @@ def build_default_initial_conditions(nx, ny):
     xx, yy = np.meshgrid(x_space, y_space)
 
     # initial velocity field is zero-valued
-    u_0_distr = np.zeros_like(xx)
-    v_0_distr = np.zeros_like(xx)
+    u_0_distr = np.zeros((xx.shape[0] + 1, xx.shape[1]))
+    v_0_distr = np.zeros((xx.shape[0], xx.shape[1] + 1))
 
     # define area where the spot is located
     spot_mask = (
@@ -40,13 +40,8 @@ def build_default_initial_conditions(nx, ny):
     p_0_distr = (
         -rho_0
         * g
-        * (yy * (1.0 - spot_mask) + (yy - 1.0 / Lambda / 2.0 * yy ** 2) * spot_mask)
+        * (yy * (spot_mask) + (yy - 1.0 / Lambda / 2.0 * yy ** 2) * (1. - spot_mask))
     )
-
-    # p_0_distr = -rho_0 * g * yy * spot_mask - rho_0 * g * (
-    #    yy - 1 / Lambda / 2 * yy ** 2
-    # ) * (1.0 - spot_mask)
-
     p_0_distr = p_0_distr / p_scale
 
     return (